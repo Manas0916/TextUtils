4ef9cc43ac56389b5e212ea477634c1298f81bf1
Manas Bajpai <manas.bajpai16@gmail.com>
2022-11-29 02:37:46 +0530

    Updates

---
+++ precache-manifest.0426eb9a8ff7eb52062a1d85262a0286.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f56834d00b5439f6d1bc",
+    "url": "/TextUtils/static/css/main.fcc73251.chunk.css"
+  },
+  {
+    "revision": "f56834d00b5439f6d1bc",
+    "url": "/TextUtils/static/js/main.f56834d0.chunk.js"
+  },
+  {
+    "revision": "95ea83b6c7492a61ef7b",
+    "url": "/TextUtils/static/js/1.95ea83b6.chunk.js"
+  },
+  {
+    "revision": "31d77044af729c125605",
+    "url": "/TextUtils/static/js/2.31d77044.chunk.js"
+  },
+  {
+    "revision": "05b54db4a4c3678b5054",
+    "url": "/TextUtils/static/js/runtime~main.05b54db4.js"
+  },
+  {
+    "revision": "449a5cd5755e2be621bc6ab9a90d3d12",
+    "url": "/TextUtils/index.html"
+  }
+];
